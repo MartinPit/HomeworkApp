87b404248c4febfd4cc7e728fe938bd33ec495a5
Martin Oliver Pitonak <martin.pit18@gmail.com>
2023-04-22 15:26:56 +0200

    fixed logout not working

--- lib/widgets/home/home_app_bar.dart
+++ lib/widgets/home/home_app_bar.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart' hide User;
 import 'package:flutter/material.dart';
+import 'package:homework_app/models/teacher.dart';
 import 'package:provider/provider.dart';
 
+import '../../models/student.dart';
 import '../../models/user.dart';
 import '../../screens/profile_screen.dart';
 
@@ -9,7 +11,8 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   final bool isStudent;
 
-  const HomeAppBar({Key? key, required this.title, required this.isStudent}) : super(key: key);
+  const HomeAppBar({Key? key, required this.title, required this.isStudent})
+      : super(key: key);
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
@@ -20,12 +23,14 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
       title: Text(title),
       actions: [
         IconButton(
-            onPressed: () =>
-                Navigator.of(context).pushNamed(ProfileScreen.routeName, arguments: isStudent),
+            onPressed: () => Navigator.of(context)
+                .pushNamed(ProfileScreen.routeName, arguments: isStudent),
             icon: const Icon(Icons.account_circle_outlined)),
         IconButton(
           onPressed: () async {
-            Provider.of<User>(context, listen: false).clear();
+            isStudent
+                ? Provider.of<Student>(context, listen: false).clear()
+                : Provider.of<Teacher>(context, listen: false).clear();
             FirebaseAuth.instance.signOut();
           },
           icon: const Icon(Icons.logout),
